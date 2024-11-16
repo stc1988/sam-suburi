6f02798f4a251f2641b7174522d58f792be7b856
STC <stc1988@users.noreply.github.com>
2024-11-16 23:31:36 +0900

    LambdaでSSMのgetとputをする

---
+++ src/lambda/update-token/app.js
@@ -0,0 +1,29 @@
+import {
+  getParamterFromSSM,
+  getParamtersFromSSM,
+  putParamterFromSSM,
+} from "./ssm.js";
+
+const SECRET_PARAM_NAME = process.env.SECRET_PARAM_NAME;
+const CLIENT_ID_PARAM_NAME = process.env.CLIENT_ID_PARAM_NAME;
+const TOKEN_PARAM_NAME = process.env.TOKEN_PARAM_NAME;
+
+export const lambdaHandler = async (event, context) => {
+  const values = await getParamtersFromSSM(
+    [SECRET_PARAM_NAME, CLIENT_ID_PARAM_NAME],
+    true
+  );
+
+  console.log(values[SECRET_PARAM_NAME], values[CLIENT_ID_PARAM_NAME]);
+
+  await putParamterFromSSM(TOKEN_PARAM_NAME, "12345", "String", true);
+
+  const response = {
+    statusCode: 200,
+    body: JSON.stringify({
+      message: "hello world",
+    }),
+  };
+
+  return response;
+};
