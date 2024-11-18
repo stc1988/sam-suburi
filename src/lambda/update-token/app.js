d6ede026fe893d487978e83bfc0d372657285b07
STC <stc1988@users.noreply.github.com>
2024-11-18 21:47:56 +0900

    依存関係の整理

--- src/lambda/update-token/app.js
+++ src/lambda/update-token/app.js
@@ -9,14 +9,19 @@ const CLIENT_ID_PARAM_NAME = process.env.CLIENT_ID_PARAM_NAME;
 const TOKEN_PARAM_NAME = process.env.TOKEN_PARAM_NAME;
 
 export const lambdaHandler = async (event, context) => {
-  const values = await getParamtersFromSSM(
-    [SECRET_PARAM_NAME, CLIENT_ID_PARAM_NAME],
-    true
-  );
+  // const values = await getParamtersFromSSM(
+  //   [SECRET_PARAM_NAME, CLIENT_ID_PARAM_NAME],
+  //   true
+  // );
 
-  console.log(values[SECRET_PARAM_NAME], values[CLIENT_ID_PARAM_NAME]);
+  // console.log(values[SECRET_PARAM_NAME], values[CLIENT_ID_PARAM_NAME]);
 
   // await putParamterFromSSM(TOKEN_PARAM_NAME, "12345", "String", true);
+
+  const res = await fetch("https://httpbin.org/get");
+  const json = await res.json()
+  console.log(json)
+
 
   const response = {
     statusCode: 200,
