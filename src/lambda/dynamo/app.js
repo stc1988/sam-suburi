9f53d8b82c522474e47e9ad341db142e93b1b0ba
STC <stc1988@users.noreply.github.com>
2024-11-20 22:43:00 +0900

    dynamo

---
+++ src/lambda/dynamo/app.js
@@ -0,0 +1,38 @@
+import {
+  putItem,
+  deleteItem,
+  updateTTL,
+  queryUserNamesByPrjIdAndMinTTL,
+} from "./dynamo.js";
+
+const CLIENT_TABLE_NAME = process.env.CLIENT_TABLE_NAME;
+
+export const lambdaHandler = async (event, context) => {
+  const currentTimestamp = Math.floor(Date.now() / 1000);
+  const ttl = currentTimestamp + 100 * 60;
+  // await putItem(CLIENT_TABLE_NAME,
+  //   {
+  //   user_name: "john_doe",
+  //   project_id: "project_123",
+  //   area_id: "area_456",
+  //   ttl: ttl}
+  //  )
+
+  // await deleteItem(CLIENT_TABLE_NAME, "john_doe");
+
+  const users = await queryUserNamesByPrjIdAndMinTTL(
+    CLIENT_TABLE_NAME,
+    "area_456",
+    currentTimestamp
+  );
+  console.log(users);
+
+  const response = {
+    statusCode: 200,
+    body: JSON.stringify({
+      message: "hello world",
+    }),
+  };
+
+  return response;
+};
---
+++ src/lambda/dynamo/dynamo.js
@@ -0,0 +1,79 @@
+import { DynamoDBClient } from "@aws-sdk/client-dynamodb";
+import {
+  PutCommand,
+  UpdateCommand,
+  DeleteCommand,
+  QueryCommand,
+} from "@aws-sdk/lib-dynamodb";
+
+const client = new DynamoDBClient({ region: process.env.AWS_REGION });
+
+export async function putItem(tableName, item) {
+  const params = {
+    TableName: tableName,
+    Item: item,
+  };
+  const result = await client.send(new PutCommand(params));
+}
+
+export async function deleteItem(tableName, userName) {
+  const params = {
+    TableName: tableName,
+    Key: { user_name: userName },
+  };
+  const result = await client.send(new DeleteCommand(params));
+}
+
+// TTLが一定以上(アクティブ)なレコードのuser_nameを抽出
+export async function queryUserNamesByMinTTL(tableName, areaId, minTTL) {
+  const params = {
+    TableName: tableName,
+    IndexName: "TtlIndex",
+    KeyConditionExpression: "area_id = :areaId AND #ttl >= :minTTL",
+    ExpressionAttributeNames: {
+      "#ttl": "ttl",
+    },
+    ExpressionAttributeValues: {
+      ":areaId": areaId,
+      ":minTTL": minTTL,
+    },
+    ProjectionExpression: "user_name",
+  };
+
+  const result = await client.send(new QueryCommand(params));
+  return result.Items.map((item) => item.user_name);
+}
+
+// TTLが一定以下(悲アクティブ)なレコードのuser_nameを抽出
+export async function queryUserNamesByMaxTTL(tableName, areaId, maxTTL) {
+  const params = {
+    TableName: tableName,
+    IndexName: "TtlIndex",
+    KeyConditionExpression: "area_id = :areaId AND #ttl =< :maxTTL",
+    ExpressionAttributeNames: {
+      "#ttl": "ttl",
+    },
+    ExpressionAttributeValues: {
+      ":areaId": areaId,
+      ":maxTTL": maxTTL,
+    },
+    ProjectionExpression: "user_name",
+  };
+
+  const result = await client.send(new QueryCommand(params));
+  return result.Items.map((item) => item.user_name);
+}
+
+// 1WCU未満のレコードサイズのため毎回putすればよいので不要
+export async function updateTTL(tableName, userName, newTTL) {
+  const params = {
+    TableName: tableName,
+    Key: { user_name: userName },
+    UpdateExpression: "SET ttl = :newTTL",
+    ExpressionAttributeValues: {
+      ":newTTL": newTTL,
+    },
+  };
+
+  const result = await client.send(new UpdateCommand(params));
+}
