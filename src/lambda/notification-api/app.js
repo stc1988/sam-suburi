d660fe0d4b92b3a9f03842fe176c9ca638015956
STC <stc1988@users.noreply.github.com>
2024-11-23 20:02:06 +0900

    iotthing

--- src/lambda/notification-api/app.js
+++ src/lambda/notification-api/app.js
@@ -1,37 +1,34 @@
-import { IoTDataPlaneClient, PublishCommand } from "@aws-sdk/client-iot-data-plane";
+import {
+  IoTDataPlaneClient,
+  PublishCommand,
+} from "@aws-sdk/client-iot-data-plane";
 
-
-// const iotEndpoint = process.env.AWS_IOT_ENDPOINT; // 環境変数に設定する
-
-const iotEndpoint = `a39gpi4t7gboc0-ats.iot.ap-northeast-1.amazonaws.com`
-
+const iotEndpoint = process.env.MQTT_ENDPOINT;
 const client = new IoTDataPlaneClient({ endpoint: `https://${iotEndpoint}` });
 
 export const lambdaHandler = async (event, context) => {
+  const topic = event.path.slice(1); //先頭の/は不要
+  const payload = event.body;
 
+  const params = {
+    topic,
+    payload: payload,
+    qos: 0,
+  };
 
-   const topic = event.path.slice(1);//先頭の/は不要
-   const payload = event.body
-
-   const params = {
-       topic,
-       payload: payload, 
-       qos: 0,
-   };
-
-   try {
-       const command = new PublishCommand(params);
-       await client.send(command);
-       console.log(`Message published to topic "${topic}" successfully.`);
-       return {
-           statusCode: 200,
-           body: JSON.stringify({ message: "Message sent successfully" }),
-       };
-   } catch (error) {
-       console.error("Error publishing message:", error);
-       return {
-           statusCode: 500,
-           body: JSON.stringify({ error: "Failed to send message" }),
-       };
-   }
+  try {
+    const command = new PublishCommand(params);
+    await client.send(command);
+    console.log(`Message published to topic "${topic}" successfully.`);
+    return {
+      statusCode: 200,
+      body: JSON.stringify({ message: "Message sent successfully" }),
+    };
+  } catch (error) {
+    console.error("Error publishing message:", error);
+    return {
+      statusCode: 500,
+      body: JSON.stringify({ error: "Failed to send message" }),
+    };
+  }
 };
